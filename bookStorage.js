53c1167eec63f2a24d44d09cdbe1c815fcdd6344
Braun <braunj2@student.douglascollege.ca>
2024-04-18 17:45:10 -0700

    fixed: schema to be exactly as shown

--- bookStorage.js
+++ bookStorage.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
-    description: {type: String, required: true},
+    description: {type: String},
 });
 
 const Book = mongoose.model("bookrecord", bookSchema);
